311776a1f187ec176376bd9564c976693b1b25d2
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 13:00:19 +0530

    Main Container Component - initial

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import MainContainer from './MainContainer'
 
 const Body = () => {
 	return (
 		<div>
 			<Sidebar />
+			<MainContainer />
 		</div>
 	)
 }
