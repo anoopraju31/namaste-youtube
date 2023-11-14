2e0d3d8c50aa0889e57a56f7db723875e88a23d6
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 14:18:36 +0530

    ButtonList Component - list mapping

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Button from './Button'
 
+const list = ['All', 'Gaming', 'Sports', 'Music', 'Live', 'News', 'Football']
+
 const ButtonList = () => {
 	return (
-		<div className='p-3 flex gap-3'>
-			<Button name='All' />
-			<Button name='Gaming' />
-			<Button name='Sports' />
-			<Button name='Music' />
-			<Button name='Live' />
-			<Button name='News' />
+		<div className='w-full overflow-auto p-3 flex gap-3'>
+			{list.map((name) => (
+				<Button key={name} name={name} />
+			))}
 		</div>
 	)
 }
