e34d26faf86b22aa717419d3e304248ac0d6e471
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 13:54:53 +0530

    appSlice created and added to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import appReducer from './appSlice'
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		app: appReducer,
+	},
 })
 
 export default store
