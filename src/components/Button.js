8426661f0ddd99e96ef624ae7b6b1e7e6a17371d
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 14:14:14 +0530

    Button Component - prop name added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ name }) => {
 	return (
 		<button className='px-5 py-2 m-2 rounded-lg bg-slate-300 outline-none'>
-			Button
+			{name}
 		</button>
 	)
 }
