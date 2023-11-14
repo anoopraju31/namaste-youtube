b3c28a42390681d3baa19e7987004bd4809c47ea
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 13:42:22 +0530

    Body Component - layout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sidebar = () => {
 	return (
-		<aside className='p-5 shadow-lg col-span-1'>
+		<aside className='p-5 shadow-lg bg-slate-200 w-60'>
 			<h2> Subscriptions </h2>
 			<ul>
 				<li> Music </li>
