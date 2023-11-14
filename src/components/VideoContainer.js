bcdebe3d0cb3caeed568536b0bc976cf9e973781
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 15:03:42 +0530

    Video Container - api data gets stored to state vaiable

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEO_LIST_API } from '../utills/constants'
 
 const VideoContainer = () => {
+	const [videos, setVideos] = useState([])
+
 	useEffect(() => {
 		getVideos()
 	}, [])
@@ -10,6 +12,9 @@ const VideoContainer = () => {
 		const res = await fetch(YOUTUBE_VIDEO_LIST_API)
 		const data = await res.json()
 
+		if (!data.items) return
+
+		setVideos(data?.items)
 		console.log(data.items)
 	}
 
