a06a18ae91ac9f67cf518dd60aa459840547c541
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 13:01:08 +0530

    Button List Component - initial

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import ButtonList from './ButtonList'
 
 const MainContainer = () => {
-	return <div>MainContainer</div>
+	return (
+		<div>
+			<ButtonList />
+		</div>
+	)
 }
 
 export default MainContainer
