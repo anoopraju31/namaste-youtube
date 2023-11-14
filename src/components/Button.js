f6c99d501432971222d3d90dbc269bb215bad842
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 14:12:18 +0530

    Button Component - style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Button = () => {
-	return <div>Button</div>
+	return (
+		<button className='px-5 py-2 m-2 rounded-lg bg-slate-300 outline-none'>
+			Button
+		</button>
+	)
 }
 
 export default Button
