730e0c186c1a299b0eefd28c8ae5fd0223181589
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 15:00:43 +0530

    Video Container - fetch data from youtube api

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YOUTUBE_VIDEO_LIST_API } from '../utills/constants'
 
 const VideoContainer = () => {
+	useEffect(() => {
+		getVideos()
+	}, [])
+
+	const getVideos = async () => {
+		const res = await fetch(YOUTUBE_VIDEO_LIST_API)
+		const data = await res.json()
+
+		console.log(data.items)
+	}
+
 	return <div>VideoContainer</div>
 }
 
