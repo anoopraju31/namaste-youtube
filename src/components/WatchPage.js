e682b358bf08fae94fd6a9fa2ec79b8ebe29833e
anoopraju31 <anoopraju31@gmail.com>
2023-11-15 23:12:50 +0530

    WatchPage - iframe added

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../redux/appSlice'
+import { useSearchParams } from 'react-router-dom'
 
 const WatchPage = () => {
+	const [searchParams] = useSearchParams()
 	const dispatch = useDispatch()
 
 	useEffect(() => {
 		dispatch(closeMenu())
 	}, [])
 
-	return <div>WatchPage</div>
+	return (
+		<div>
+			<iframe
+				width='560'
+				height='315'
+				src={`https://www.youtube.com/embed/${searchParams.get(
+					'v',
+				)}?&autoplay=1&rel=0`}
+				title='YouTube video player'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen></iframe>
+		</div>
+	)
 }
 
 export default WatchPage
