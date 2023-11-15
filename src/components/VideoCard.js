847ece2bd29a4e6f65ec50545980c94ec1990dab
anoopraju31 <anoopraju31@gmail.com>
2023-11-15 18:34:56 +0530

    Video Card - styled

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { YOUTUBE_CHANNEL_PROFILE_API_URL } from '../utills/constants'
 
-const VideoCard = () => {
-	return <div>VideoCard</div>
+const VideoCard = ({ info }) => {
+	console.log(info)
+
+	if (!info) return null
+
+	const { snippet, statistics } = info
+	const { channelTitle, title, thumbnails } = snippet
+
+	return (
+		<div className='w-full'>
+			<div className='w-full'>
+				<img
+					className='w-full rounded-xl'
+					src={thumbnails?.medium?.url}
+					alt='video thumbnail'
+				/>
+			</div>
+
+			<div className='w-full px-2 pt-2'>
+				<h3 className='truncate font-medium'> {title} </h3>
+				<p className=''>{channelTitle}</p>
+
+				<div className='flex gap-2 text-sm'>
+					<p> {statistics.viewCount} views </p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default VideoCard
