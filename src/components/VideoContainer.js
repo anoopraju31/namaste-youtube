57fdaaba761bcfdb0c58030fe34aa1ba5d00ed3a
anoopraju31 <anoopraju31@gmail.com>
2023-11-15 18:35:59 +0530

    Tailwind CSS - Container Query plugin added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEO_LIST_API } from '../utills/constants'
+import VideoCard from './VideoCard'
 
 const VideoContainer = () => {
 	const [videos, setVideos] = useState([])
@@ -15,10 +16,46 @@ const VideoContainer = () => {
 		if (!data.items) return
 
 		setVideos(data?.items)
-		console.log(data.items)
 	}
 
-	return <div>VideoContainer</div>
+	if (!videos) return null
+
+	return (
+		<div className='@container/main p-3'>
+			<div className='grid grid-cols-1 @sm/main:grid-cols-1 @lg/main:grid-cols-2 @2xl/main:grid-cols-3 @4xl/main:grid-cols-4 gap-3'>
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+				<VideoCard info={videos[0]} />
+			</div>
+		</div>
+	)
 }
 
 export default VideoContainer
