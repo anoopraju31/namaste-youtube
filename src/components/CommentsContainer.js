8f838401e019ce71ee71085e6b478ede252a57c4
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 21:27:59 +0530

    Comment Container - dummy comment data added

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,7 +1,79 @@
 import React from 'react'
 
+const commentsData = [
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [],
+	},
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [
+			{
+				name: 'Anoop Raju',
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+				replies: [],
+			},
+			{
+				name: 'Anoop Raju',
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+				replies: [],
+			},
+		],
+	},
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [
+			{
+				name: 'Anoop Raju',
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+				replies: [],
+			},
+		],
+	},
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [
+			{
+				name: 'Anoop Raju',
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+				replies: [
+					{
+						name: 'Anoop Raju',
+						text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+						replies: [
+							{
+								name: 'Anoop Raju',
+								text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+								replies: [],
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [],
+	},
+	{
+		name: 'Anoop Raju',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus.',
+		replies: [],
+	},
+]
+
 const CommentsContainer = () => {
-	return <div>CommentsContainer</div>
+	return (
+		<div className='m-5 p-2'>
+			<h1 className='text-2xl font-bold'> Comments </h1>
+		</div>
+	)
 }
 
 export default CommentsContainer
