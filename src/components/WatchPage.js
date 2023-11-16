c7512ae00dc853eb28e41b1b5a8900537fa822cd
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 16:13:13 +0530

    Watch Page - Layout change

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -13,20 +13,28 @@ const WatchPage = () => {
 	}, [])
 
 	return (
-		<main className=''>
-			<div>
-				<iframe
-					width='560'
-					height='315'
-					src={`https://www.youtube.com/embed/${searchParams.get(
-						'v',
-					)}?&autoplay=1&rel=0`}
-					title='YouTube video player'
-					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-					allowFullScreen></iframe>
+		<main className='w-full px-4 flex gap-4'>
+			<div className='w-full lg:w-2/3 flex flex-col gap-4'>
+				<div className='w-full'>
+					<iframe
+						width='560'
+						className='w-full aspect-video rounded-lg'
+						src={`https://www.youtube.com/embed/${searchParams.get(
+							'v',
+						)}?&autoplay=1&mute=0&loop=1&controls=1&rel=0&showinfo=0`}
+						title='YouTube video player'
+						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+						allowFullScreen></iframe>
+				</div>
+
+				<div className='w-full lg:hidden'>relate videos</div>
+
+				<div className='w-full'>
+					<CommentsContainer />
+				</div>
 			</div>
 
-			<CommentsContainer />
+			<div className='hidden lg:block lg:w-1/3'>relate videos</div>
 		</main>
 	)
 }
