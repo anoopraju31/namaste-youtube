615798f3285b8b98df078ba16df7e2fbff8b74c1
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 05:26:22 +0530

    Live Chat Component - styled

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -3,9 +3,39 @@ import ChatMessage from './ChatMessage'
 
 const LiveChat = () => {
 	return (
-		<div className='w-full h-[600px] p-2 bg-slate-100 border border-black rounded-lg'>
-			<ChatMessage name='Askshay Saini' message='This namaste react live' />
-		</div>
+		<section className='w-full p-2 bg-slate-100 border border-black rounded-lg'>
+			<div className='px-4 pb-2'>
+				<h2 className='text-lg font-bold'> Live Chat </h2>
+			</div>
+			<div className='h-[600px] py-2 overflow-auto'>
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+				<ChatMessage name='Askshay Saini' message='This namaste react live' />
+			</div>
+		</section>
 	)
 }
 
