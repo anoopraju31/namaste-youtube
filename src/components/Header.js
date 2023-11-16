17b44a763c419c12df2ab971990a8e6c4dbdfb97
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 22:09:32 +0530

    Search Bar Component - styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { RxHamburgerMenu } from 'react-icons/rx'
-import { IoMdSearch } from 'react-icons/io'
 import user from '../assests/img/user.png'
 import { toggleMenu } from '../redux/appSlice'
-import { Link } from 'react-router-dom'
-import { YOUTUBE_SEARCH_SUGGESTIONS_API_URL } from '../utills/constants'
-import {
-	cacheSuggestions,
-	removeSuggestions,
-	searchQueryChange,
-} from '../redux/searchSlice'
+import SearchBar from './SearchBar'
 
 const Header = () => {
-	const [suggestions, setSuggestions] = useState([])
-	const [showSuggestions, setShowSuggestions] = useState(false)
-	const searchQuery = useSelector((state) => state.search.searchQuery)
-	const cachedSuggestions = useSelector(
-		(state) => state.search.cachedSuggestions,
-	)
 	const dispatch = useDispatch()
-
-	// Debouncing
-	useEffect(() => {
-		const timer = setTimeout(() => getSearchSuggestions(), 200)
-
-		return () => {
-			clearTimeout(timer)
-		}
-	}, [searchQuery])
-
-	const getSearchSuggestions = async () => {
-		if (!searchQuery) {
-			setSuggestions([])
-			setShowSuggestions(false)
-			dispatch(removeSuggestions())
-			return
-		}
-
-		if (cachedSuggestions[searchQuery]) {
-			setShowSuggestions(cachedSuggestions[searchQuery])
-			return
-		}
-
-		const res = await fetch(YOUTUBE_SEARCH_SUGGESTIONS_API_URL + searchQuery)
-		const data = await res.json()
-
-		setSuggestions(data[1])
-		dispatch(cacheSuggestions({ searchQuery, suggestion: data[1] }))
-		setShowSuggestions(true)
-	}
 
 	const toggleMenuHandler = () => {
 		dispatch(toggleMenu())
-	}
-
-	const handleSearchQueryChange = (e) => {
-		dispatch(searchQueryChange(e.target.value))
 	}
 
 	return (
@@ -81,31 +34,7 @@ const Header = () => {
 
 			{/* Search Bar */}
 			<div className='col-span-10 px-10 flex justify-center items-center'>
-				<div className='relative w-2/3 max-w-lg'>
-					<input
-						className='w-full px-4 py-2 border border-gray-400 outline-none rounded-l-full'
-						type='text'
-						value={searchQuery}
-						onChange={handleSearchQueryChange}
-						placeholder='search'
-						onFocus={() => setShowSuggestions(true)}
-						onBlur={() => setShowSuggestions(false)}
-					/>
-
-					{showSuggestions && suggestions.length > 0 && (
-						<ul className='absolute top-12 w-full min-w-fit min-w-lg py-2 bg-gray-200 rounded-xl z-50 flex flex-col gap-1'>
-							{suggestions?.map((suggestion) => (
-								<l1 className='px-4 py-1 cursor-pointer flex gap-2 items-center hover:bg-gray-50 whitespace-nowrap'>
-									<IoMdSearch size={20} />
-									<p className='font-bold'>{suggestion}</p>
-								</l1>
-							))}
-						</ul>
-					)}
-				</div>
-				<button className='px-4 py-2 border border-gray-400 outline-none  rounded-r-full'>
-					<IoMdSearch size={24} />
-				</button>
+				<SearchBar />
 			</div>
 
 			{/* user */}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { IoMdSearch } from 'react-icons/io'
+import {
+	cacheSuggestions,
+	removeSuggestions,
+	searchQueryChange,
+} from '../redux/searchSlice'
+import { YOUTUBE_SEARCH_SUGGESTIONS_API_URL } from '../utills/constants'
 
 const SearchBar = () => {
-	return <div>SearchBar</div>
+	const [suggestions, setSuggestions] = useState([])
+	const [showSuggestions, setShowSuggestions] = useState(false)
+	const searchQuery = useSelector((state) => state.search.searchQuery)
+	const cachedSuggestions = useSelector(
+		(state) => state.search.cachedSuggestions,
+	)
+
+	const dispatch = useDispatch()
+
+	// Debouncing
+	useEffect(() => {
+		const timer = setTimeout(() => getSearchSuggestions(), 200)
+
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [searchQuery])
+
+	const getSearchSuggestions = async () => {
+		if (!searchQuery) {
+			setSuggestions([])
+			setShowSuggestions(false)
+			dispatch(removeSuggestions())
+			return
+		}
+
+		if (cachedSuggestions[searchQuery]) {
+			setShowSuggestions(cachedSuggestions[searchQuery])
+			return
+		}
+
+		const res = await fetch(YOUTUBE_SEARCH_SUGGESTIONS_API_URL + searchQuery)
+		const data = await res.json()
+
+		setSuggestions(data[1])
+		dispatch(cacheSuggestions({ searchQuery, suggestion: data[1] }))
+		setShowSuggestions(true)
+	}
+
+	const handleSearchQueryChange = (e) => {
+		dispatch(searchQueryChange(e.target.value))
+	}
+	return (
+		<>
+			<div className='relative w-2/3 max-w-lg'>
+				<input
+					className='w-full px-4 py-2 border border-gray-400 outline-none rounded-l-full'
+					type='text'
+					value={searchQuery}
+					onChange={handleSearchQueryChange}
+					placeholder='search'
+					onFocus={() => setShowSuggestions(true)}
+					onBlur={() => setShowSuggestions(false)}
+				/>
+
+				{showSuggestions && suggestions.length > 0 && (
+					<ul className='absolute top-12 w-full min-w-fit min-w-lg py-2 bg-gray-200 rounded-xl z-50 flex flex-col gap-1'>
+						{suggestions?.map((suggestion) => (
+							<l1 className='px-4 py-1 cursor-pointer flex gap-2 items-center hover:bg-gray-50 whitespace-nowrap'>
+								<IoMdSearch size={20} />
+								<p className='font-bold'>{suggestion}</p>
+							</l1>
+						))}
+					</ul>
+				)}
+			</div>
+			<button className='px-4 py-2 border border-gray-400 outline-none  rounded-r-full'>
+				<IoMdSearch size={24} />
+			</button>
+		</>
+	)
 }
 
 export default SearchBar
