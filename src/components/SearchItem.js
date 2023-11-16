15c5472b437e08c0bb3ebee02492434a88b041f9
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 22:36:21 +0530

    Search Item Component - handle click event and navigate to search page

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { IoMdSearch } from 'react-icons/io'
 
 const SearchItem = ({ suggestion }) => {
+	const navigate = useNavigate()
+
+	const handleClick = () => {
+		navigate('/result?search_query=helloween')
+	}
 	return (
-		<l1 className='px-4 py-1 cursor-pointer flex gap-2 items-center hover:bg-gray-50 whitespace-nowrap'>
+		<li
+			onClick={handleClick}
+			className='px-4 py-1 cursor-pointer flex gap-2 items-center hover:bg-gray-50 whitespace-nowrap'>
 			<IoMdSearch size={20} />
 			<p className='font-bold'>{suggestion}</p>
-		</l1>
+		</li>
 	)
 }
 
