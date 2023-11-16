980f741219563a8ae63a98300b21024e10500e73
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 11:34:03 +0530

    Search suggesstions addedf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { IoMdSearch } from 'react-icons/io'
+import { PiClockCounterClockwiseLight } from 'react-icons/pi'
 import user from '../assests/img/user.png'
 import { toggleMenu } from '../redux/appSlice'
 import { Link } from 'react-router-dom'
+import { YOUTUBE_SEARCH_API } from '../utills/constants'
 
 const Header = () => {
+	const [searchQuery, setSearchQuery] = useState('')
+	const [suggestions, setSuggestions] = useState([])
 	const dispatch = useDispatch()
+
+	// Debouncing
+	useEffect(() => {
+		const timer = setTimeout(() => getSearchSuggestions(), 200)
+
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [searchQuery])
+
+	const getSearchSuggestions = async () => {
+		if (!searchQuery) {
+			setSuggestions([])
+			return
+		}
+
+		const res = await fetch(YOUTUBE_SEARCH_API + searchQuery)
+		const data = await res.json()
+
+		setSuggestions(data[1])
+	}
 
 	const toggleMenuHandler = () => {
 		dispatch(toggleMenu())
@@ -34,12 +59,27 @@ const Header = () => {
 			</div>
 
 			{/* Search Bar */}
-			<div className='col-span-10 px-10 flex items-center justify-center'>
-				<input
-					className='w-2/3 max-w-lg px-4 py-2 border border-gray-400 outline-none rounded-l-full'
-					type='text'
-					placeholder='search'
-				/>
+			<div className='col-span-10 px-10 flex justify-center items-center'>
+				<div className='relative w-2/3 max-w-lg'>
+					<input
+						className='w-full px-4 py-2 border border-gray-400 outline-none rounded-l-full'
+						type='text'
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
+						placeholder='search'
+					/>
+
+					{suggestions.length > 0 && (
+						<ul className='absolute top-12 w-full min-w-fit min-w-lg py-2 bg-gray-200 rounded-xl z-50 flex flex-col gap-1'>
+							{suggestions?.map((suggestion) => (
+								<l1 className='px-4 py-1 cursor-pointer flex gap-2 items-center hover:bg-gray-50 whitespace-nowrap'>
+									<PiClockCounterClockwiseLight size={20} />
+									<p className='font-bold'>{suggestion}</p>
+								</l1>
+							))}
+						</ul>
+					)}
+				</div>
 				<button className='px-4 py-2 border border-gray-400 outline-none  rounded-r-full'>
 					<IoMdSearch size={24} />
 				</button>
--- src/utills/constants.js
+++ src/utills/constants.js
@@ -3,3 +3,6 @@ export const YOUTUBE_VIDEO_LIST_API =
 	process.env.REACT_APP_YOUTUBE_API_KEY
 export const YOUTUBE_CHANNEL_PROFILE_API_URL = (channelId) =>
 	`https://www.googleapis.com/youtube/v3/channels?part=snippet&id+${channelId}&fields=items%2Fsnippet%2Fthumbnails&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+
+export const YOUTUBE_SEARCH_API =
+	'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
