a8b314b67bdba3d014ed0baf173e5f8ba5135a6f
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 22:54:25 +0530

    SearchPage - search result retrival

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { YOUTUBE_SEARCH_API_URL } from '../utills/constants'
 
 const SearchPage = () => {
+	const [results, setResults] = useState([])
 	const [searchParams] = useSearchParams()
-	console.log(searchParams.get('search_query'))
-	return <main className='px-4 mt-4'>SearchPage</main>
+
+	useEffect(() => {
+		getSearchResults()
+	}, [])
+
+	const getSearchResults = async () => {
+		const searchQuery = searchParams.get('search_query')
+		const res = await fetch(YOUTUBE_SEARCH_API_URL(searchQuery))
+		const data = await res.json()
+
+		if (!data?.items) return
+
+		setResults(data.items)
+	}
+
+	return (
+		<main className='px-4 mt-4'>
+			{results.map((result) => (
+				<p className='my-2' key={result.etag}>
+					{' '}
+					{result.snippet.title}{' '}
+				</p>
+			))}
+		</main>
+	)
 }
 
 export default SearchPage
