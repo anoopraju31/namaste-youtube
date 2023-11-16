f72fc619e54d1a990a4c2fd7a7a4839da3b8e3e0
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 05:10:33 +0530

    Chat Message Component - initial

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ChatMessage from './ChatMessage'
 
 const LiveChat = () => {
 	return (
 		<div className='w-full h-[600px] p-2 bg-slate-100 border border-black rounded-lg'>
-			LiveChat
+			<ChatMessage />
 		</div>
 	)
 }
