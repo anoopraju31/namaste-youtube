a5b0fa7affe76b89492e485cf140c492e3b2bb85
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 15:12:23 +0530

    Comments Container - initila

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../redux/appSlice'
 import { useSearchParams } from 'react-router-dom'
+import CommentsContainer from './CommentsContainer'
 
 const WatchPage = () => {
 	const [searchParams] = useSearchParams()
@@ -12,17 +13,21 @@ const WatchPage = () => {
 	}, [])
 
 	return (
-		<div>
-			<iframe
-				width='560'
-				height='315'
-				src={`https://www.youtube.com/embed/${searchParams.get(
-					'v',
-				)}?&autoplay=1&rel=0`}
-				title='YouTube video player'
-				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-				allowFullScreen></iframe>
-		</div>
+		<main className=''>
+			<div>
+				<iframe
+					width='560'
+					height='315'
+					src={`https://www.youtube.com/embed/${searchParams.get(
+						'v',
+					)}?&autoplay=1&rel=0`}
+					title='YouTube video player'
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+					allowFullScreen></iframe>
+			</div>
+
+			<CommentsContainer />
+		</main>
 	)
 }
 
