b6af2c99747de9032f6ca20ddcb3fccfc82506b6
anoopraju31 <anoopraju31@gmail.com>
2023-11-16 23:59:52 +0530

    Custom hook - useSearchresults created

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { YOUTUBE_SEARCH_API_URL } from '../utills/constants'
 import VideoCard from './VideoCard'
+import useSearchResults from '../hooks/useSearchResults'
 
 const SearchPage = () => {
-	const [results, setResults] = useState([])
-	const [searchParams] = useSearchParams()
-
-	useEffect(() => {
-		getSearchResults()
-	}, [])
-
-	const getSearchResults = async () => {
-		const searchQuery = searchParams.get('search_query')
-		const res = await fetch(YOUTUBE_SEARCH_API_URL('snippet', searchQuery))
-		const data = await res.json()
-
-		if (!data?.items) return
-
-		setResults(data.items)
-	}
+	const results = useSearchResults()
 
 	return (
 		<main className='px-4 mt-4 grid  gap-4'>
--- src/hooks/useSearchResults.js
+++ src/hooks/useSearchResults.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { YOUTUBE_SEARCH_API_URL } from '../utills/constants'
+import { useSearchParams } from 'react-router-dom'
 
 const useSearchResults = () => {
-	return <div>useSearchResults</div>
+	const [searchParams] = useSearchParams()
+	const [results, setResults] = useState([])
+
+	useEffect(() => {
+		getSearchResults()
+	}, [])
+
+	const getSearchResults = async () => {
+		const searchQuery = searchParams.get('search_query')
+		const res = await fetch(YOUTUBE_SEARCH_API_URL('snippet', searchQuery))
+		const data = await res.json()
+
+		if (!data?.items) return
+
+		setResults(data.items)
+	}
+
+	return results
 }
 
 export default useSearchResults
