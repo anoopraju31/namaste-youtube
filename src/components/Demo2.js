58d318211bcc6b2718d943e01f70c8c4f6853c55
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 20:19:48 +0530

    Demo 2 Component - styled refactor

--- src/components/Demo2.js
+++ src/components/Demo2.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Demo2 = () => {
+	let x = 10
 	return (
 		<div className='mx-4 p-6 bg-slate-500 border border-black w-96 h-96'>
-			Demo2
+			<div>
+				<h1 className='font-bold text-xl my-4'> Let = {x}</h1>
+				<button
+					className='px-6 py-2 bg-green-500'
+					onClick={() => {
+						x += 1
+					}}>
+					{' '}
+					Increase X{' '}
+				</button>
+			</div>
 		</div>
 	)
 }
