494492f1b5120c474e99897bb25c93ed634b2c68
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 16:55:09 +0530

    Chat Form - refactor

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -1,29 +1,7 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addMessage } from '../redux/chatSlics'
+import useChatForm from '../hooks/useChatForm'
 
 const ChatForm = () => {
-	const [liveMessage, setLiveMessage] = useState('')
-	const dispatch = useDispatch()
-
-	const handleChange = (e) => {
-		setLiveMessage(e.target.value)
-	}
-
-	const handleSend = (e) => {
-		e.preventDefault()
-
-		if (!liveMessage) return
-
-		dispatch(
-			addMessage({
-				name: 'Anoop raju',
-				message: liveMessage,
-			}),
-		)
-
-		setLiveMessage('')
-	}
+	const { liveMessage, handleChange, handleSend } = useChatForm()
 
 	return (
 		<form className='p-4 flex' onSubmit={handleSend}>
--- src/hooks/useChatForm.js
+++ src/hooks/useChatForm.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addMessage } from '../redux/chatSlics'
 
 const useChatForm = () => {
-	return <div>useChatForm</div>
+	const [liveMessage, setLiveMessage] = useState('')
+	const dispatch = useDispatch()
+
+	const handleChange = (e) => {
+		setLiveMessage(e.target.value)
+	}
+
+	const handleSend = (e) => {
+		e.preventDefault()
+
+		if (!liveMessage) return
+
+		dispatch(
+			addMessage({
+				name: 'Anoop raju',
+				message: liveMessage,
+			}),
+		)
+
+		setLiveMessage('')
+	}
+
+	return {
+		liveMessage,
+		handleChange,
+		handleSend,
+	}
 }
 
 export default useChatForm
