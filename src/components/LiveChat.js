767dada5bdb8592fb45ea38f612d6f886c707e13
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 16:34:46 +0530

    Live Chat - Form added

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../redux/chatSlics'
 import ChatMessage from './ChatMessage'
 import { generateRandomName, makeRandomMessage } from '../utills/helper'
 
 const LiveChat = () => {
+	const [liveMessage, setLiveMessage] = useState('')
 	const messages = useSelector((state) => state.chat.messages)
 	const dispatch = useDispatch()
 
@@ -26,6 +27,10 @@ const LiveChat = () => {
 		}
 	}, [])
 
+	const handleChange = (e) => {
+		setLiveMessage(e.target.value)
+	}
+
 	return (
 		<section className='w-full px-2 py-6 bg-slate-100 rounded-lg'>
 			<div className='px-4 pb-2'>
@@ -39,6 +44,15 @@ const LiveChat = () => {
 						message={message.message}
 					/>
 				))}
+			</div>
+			<div className='p-4 flex'>
+				<input
+					className='w-full bg-slate-200 rounded-l-xl px-4 py-3 outline-none'
+					type='text'
+					value={liveMessage}
+					onChange={handleChange}
+				/>
+				<button className='px-4 rounded-r-xl bg-gray-500'> send </button>
 			</div>
 		</section>
 	)
