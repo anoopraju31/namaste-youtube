232000116e9a9b57a702b97c2cbfad8bc750992a
anoopraju31 <anoopraju31@gmail.com>
2023-11-17 20:24:57 +0530

    Demo 2 Component - y state added

--- src/components/Demo2.js
+++ src/components/Demo2.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo2 = () => {
+	const [y, setY] = useState(0)
 	let x = 10
+
 	return (
 		<div className='mx-4 p-6 bg-slate-500 border border-black w-96 h-96'>
 			<div>
@@ -10,9 +12,23 @@ const Demo2 = () => {
 					className='px-6 py-2 bg-green-500'
 					onClick={() => {
 						x += 1
+
+						console.log(x)
 					}}>
 					{' '}
 					Increase X{' '}
+				</button>
+			</div>
+
+			<div>
+				<h1 className='font-bold text-xl my-4'> State = {y}</h1>
+				<button
+					className='px-6 py-2 bg-green-500'
+					onClick={() => {
+						setY(y + 1)
+					}}>
+					{' '}
+					Increase Y{' '}
 				</button>
 			</div>
 		</div>
